9eeed9f0ce2c78bb92ed32ecfa323dd2c65fc163
Tom Tyler <tom.tyler@live.co.uk>
2020-12-07 00:47:14 +0000

    change margin and padding of tool card

--- src/components/tools/ToolIcon.js
+++ src/components/tools/ToolIcon.js
@@ -4,9 +4,8 @@ import {Link} from '@reach/router';
 const ToolIcon = (props) => {
 
     return (
-        <Link className={`link col-2 m-3 p-0`} to={props.to}>
-        <div className={`tool-card hvr-shadow hvr-float ${props.colour}`}>
-            
+        <Link className={`link col-3 m-0 p-3`} to={props.to}>
+            <div className={`tool-card hvr-shadow hvr-float ${props.colour}`}>  
 
                 <div className={`d-flex justify-content-center`}>
                     
@@ -17,9 +16,8 @@ const ToolIcon = (props) => {
                 </div>
 
                 <h5 className="text-center mt-0 mb-4">{props.title}</h5> 
-
-            
-        </div>
+                
+            </div>
         </Link>
     )
 
